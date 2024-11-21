a1fb92e51eb21b49fd1e3135adec7a57b22d8be8
xiufeng he <hexiufeng@xiufengdeMacBook-Pro.local>
2024-11-21 19:07:49 +0800

    add ext info

--- commons/base_context.go
+++ commons/base_context.go
@@ -24,11 +24,14 @@ type QuickInfo struct {
 }
 
 type UserInfo struct {
-	Uid       int64
-	Roles     int64
-	OpId      int64
-	CompanyId int64
-	TokenId   int64
+	Uid         int64
+	UserName    string
+	Mobile      string
+	Roles       int64
+	OpId        int64
+	CompanyId   int64
+	CompanyName string
+	TokenId     int64
 }
 
 func (info *UserInfo) AccountType() int {
