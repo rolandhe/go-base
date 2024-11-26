fd3c62bd9755f9e629fffd5ec45b9ab7416a485b
xiufeng he <hexiufeng@xiufengdeMacBook-Pro.local>
2024-11-26 21:46:15 +0800

    add company id support

--- commons/account_type.go
+++ commons/account_type.go
@@ -6,3 +6,21 @@ const (
 	CAccount    = 15
 	MaskAccount = 0x3F
 )
+
+const (
+	CommonCompany   = 61
+	PracticeCompany = 17
+	PlatformCompany = 3
+)
+
+func GetPlatformCompanyId() int64 {
+	return uniqAccountId(1, PlatformCompany)
+}
+
+func GetPracticeCompanyId() int64 {
+	return uniqAccountId(2, PracticeCompany)
+}
+
+func uniqAccountId(accountId int64, accountType int) int64 {
+	return accountId<<6 | int64(accountType)
+}
