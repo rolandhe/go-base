c4ad1b7f10fd766b80afdfc9fec83c08b1aaafa3
roland <rolandhe@qq.com>
2026-03-27 20:05:40 +0800

    add from point

--- swiss_kit/point_utils.go
+++ swiss_kit/point_utils.go
@@ -3,3 +3,7 @@ package swiss_kit
 func ToPoint[T int64 | int32 | int16 | int8 | float64 | float32 | string](v T) *T {
 	return &v
 }
+
+func FromPoint[T int64 | int32 | int16 | int8 | float64 | string](v *T) T {
+	return *v
+}
